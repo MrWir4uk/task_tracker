ebad2217445867452625292e084d5a728bb4f77c
MrWir4uck <matviiv.nazar2010@gmail.com>
2025-11-27 20:33:06 +0200

    first commit

---
+++ tast_tracer_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TastTracerAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tast_tracer_app'
---
+++ tast_tracer_app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('Новий', 'Новий'),
+        ('В процесі', 'В процесі'),
+        ('Завершено', 'Завершено'),
+    ]
+    PRIORITY_CHOICES = [
+        ('Низький', 'Низький'), 
+        ('Середній', 'Середній'), 
+        ('Високий', 'Високий'),
+    ]
+    title = models.CharField(max_length=200, verbose_name='Назва завдання')
+    description = models.TextField(verbose_name='Опис завдання', blank=True, null=True)
+    deadline = models.DateTimeField(verbose_name='Крайній термін', blank=True, null=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, default='Новий' ,verbose_name='Статус завдання')
+    priority = models.CharField(choices=PRIORITY_CHOICES, max_length=50, default='Низький' ,verbose_name='Пріоритет завдання')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+    
+
+class Coment(models.Model):
+    content = models.TextField(verbose_name='Коментар')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    media = models.FileField(upload_to='comments_media/', blank=True, null=True, verbose_name='Медіа файли')
+
